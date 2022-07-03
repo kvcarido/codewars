f2273ae5df5c9a7b307b775119030204aa00bea2
Kayleen Carido <kvcarido@gmail.com>
2022-07-02 23:37:46 -0700

    7kyu mumbling (in progress)

--- challenges/07-2022-jul/07-02-2022/2/script.js
+++ challenges/07-2022-jul/07-02-2022/2/script.js
@@ -1,12 +1,27 @@
-// Challenge Title
-// Kyu
-// Challenge link
+// Mumbling
+// 7 Kyu
+// https://www.codewars.com/kata/5667e8f4e3f572a8f2000039
 
+const accum = s => {
+    let arr = s.toLowerCase().split('')
+    let ans = []
+
+    arr.forEach((letter, index) => {
+        let counter = index + 1
+        ans.push(letter.repeat(counter))
+    })
+
+    /**************************************/
+    /* COME BACK AND FIGURE THIS PART OUT */
+    /**************************************/
+    for (let letters of ans) {
+        letters.charAt(0).toUpperCase()
+    }
+
+    return ans.join('-')
+}
 
 // Tests
-
-
-// Pseudo code
-/*
-
-*/
+console.log(accum("ABCDE")) // "A-Bb-Ccc-Dddd-Eeeee"
+// console.log(accum("ZpglnRxqenU")) // "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu"
+// console.log(accum()) // 
