05e6f0e9e93639e5b9f5b185bc17d549aed05356
Kayleen Carido <kvcarido@gmail.com>
2022-07-15 21:57:25 -0700

    8kyu warm ups - as many as you can

---
+++ challenges/07-2022-jul/07-15-2022/script.js
@@ -0,0 +1,11 @@
+// Returning String
+// 8 Kyu
+// https://www.codewars.com/kata/55a70521798b14d4750000a4
+
+const greet = name => {
+    return `Hello, ${name} how are you doing today?`
+}
+
+
+// Tests
+console.log(greet("Ryan")) // "Hello, Ryan how are you doing today?"
---
+++ challenges/07-2022-jul/07-15-2022/script2.js
@@ -0,0 +1,8 @@
+// Beginner series #2 clock
+// 8 Kyu
+// https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a
+
+const
+
+
+// Tests
