bb140b3d8cb8792b070249159f0fb5ddf00667a2
Kayleen Carido <kvcarido@gmail.com>
2022-07-15 22:01:27 -0700

    8kyu sparring - clock

--- challenges/07-2022-jul/07-15-2022/script2.js
+++ challenges/07-2022-jul/07-15-2022/script2.js
@@ -2,7 +2,12 @@
 // 8 Kyu
 // https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a
 
-const
+const past = (h, m, s) => {
+    return ((h * 60 * 60) + (m * 60) + s) * 1000
+}
 
 
 // Tests
+console.log(past(0,1,1)) // 61000
+console.log(past(1,1,1)) // 3661000
+console.log(past(0,0,0)) // 0
