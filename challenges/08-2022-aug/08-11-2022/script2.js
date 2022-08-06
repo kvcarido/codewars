1ae39cc63dda69dd4e70564b750058a7a8219415
Kayleen Carido <kvcarido@gmail.com>
2022-08-05 18:41:19 -0700

    8kyu opposites attract

--- challenges/08-2022-aug/08-11-2022/script2.js
+++ challenges/08-2022-aug/08-11-2022/script2.js
@@ -1,11 +1,13 @@
-// Unique in order
-// 6 Kyu
-// Link
+// Opposites attract
+// 8 Kyu
+// https://www.codewars.com/kata/555086d53eac039a2a000083/
 
-
+const lovefunc = (flower1, flower2) => {
+    return (flower1 + flower2) % 2 === 0 ? false : true
+}
 
 // Tests
-
-/* Pseudo Code
-
-*/
+console.log(lovefunc(1,4)) // true
+console.log(lovefunc(2,2)) // false
+console.log(lovefunc(0,1)) // true
+console.log(lovefunc(0,0)) // false
