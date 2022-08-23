258b4a58141320415f33074360607c9be7c4a4e0
Kayleen Carido <kvcarido@gmail.com>
2022-08-22 22:54:16 -0700

    8kyu cockroach speed

---
+++ challenges/08-2022-aug/08-22-2022/script.js
@@ -0,0 +1,10 @@
+// Beginner Series #4 Cockroach
+// 8 Kyu
+// https://www.codewars.com/kata/55fab1ffda3e2e44f00000c6
+
+const roachSpeed = s => {
+    return Math.floor(s / 0.036)
+}
+
+// Tests
+console.log(roachSpeed(1.08)) // 30
