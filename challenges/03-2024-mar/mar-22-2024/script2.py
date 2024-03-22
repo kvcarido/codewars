3428aa07bbb3bbcf34e8cbb5268540b8d7615348
Kayleen Carido <kvcarido@gmail.com>
2024-03-22 14:52:05 -0400

    python 7kyu

---
+++ challenges/03-2024-mar/mar-22-2024/script2.py
@@ -0,0 +1,8 @@
+#!/bin/usr/env python
+
+# Challenge: Grasshopper - Summation
+# Link: https://www.codewars.com/kata/55d24f55d7dd296eb9000030
+# Kyu: 8
+
+def summanation(num):
+    return sum(range(num + 1))
