4ce566545189ff35e4c30aed445a1f7d2515b940
Kayleen Carido <kvcarido@gmail.com>
2024-03-29 14:46:52 -0400

    python 7kyu

--- challenges/03-2024-mar/mar-22-2024/script5.py
+++ challenges/03-2024-mar/mar-22-2024/script5.py
@@ -4,6 +4,5 @@
 # Link: https://www.codewars.com/kata/545a4c5a61aa4c6916000755
 # Kyu: 7
 
-def middle_element(list):
-    ordered = sorted(list)
-    return ordered[len(ordered) // 2]
+def gimme(input_array):
+    return input_array.index(sorted(input_array)[1])
